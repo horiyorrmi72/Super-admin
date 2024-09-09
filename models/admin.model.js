50f1f9bb968c4756d9dfbb04f84ccd9e7cd77125
yusuf <horiyorrmi72@gmail.com>
2024-09-09 13:57:28 +0100

    admin Schema

---
+++ models/admin.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+
+const adminSchema = new Schema({
+	firstName: { type: String, required: true, trim: true },
+	lastName: { type: String, required: true, trim: true },
+	email: { type: String, required: true, trim: true, unique: true },
+	password: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 4,
+		select: false,
+	},
+	phoneNumber: { type: String,trim: true, unique: true },
+	role: {
+		type: String,
+		required: true,
+		trim: true,
+		enum: ['admin', 'super-admin', 'super-user'],
+		select:false
+	},
+	permissions: {
+		type: String,
+		required: true,
+		trim: true,
+		enum: ['read-only', 'read-write', 'read-write-delete'],
+		default: 'read-only',
+		select:false
+	},
+});
+const Admin = mongoose.model('Admin', adminSchema);
+export default Admin;
