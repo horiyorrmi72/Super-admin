7a7f28fa604d89d00a808d3f36207d92e29b4601
yusuf <horiyorrmi72@gmail.com>
2024-09-14 06:47:12 +0100

    fixed passport import and call back

--- config/passport.config.js
+++ config/passport.config.js
@@ -1,20 +1,23 @@
+import passport from 'passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
-import Admin from '../models/admin.model';
-const opts = {}
+import Admin from '../models/admin.model.js';
+const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.SECRET;
 
-passport.use(new Strategy(opts, (err, done) => {
-    Admin.findOne({ id: jwt_payload._id,function (err, admin) {
-        if(err) {
-            return done(err, false);
-        }
-        if(admin) {
-            return done(null, admin);
-        }else{
-            return done(null, false);
-        }
-    }})
-}))
+passport.use(
+	new Strategy(opts, (jwt_payload, done) => {
+		Admin.findOne({ _id: jwt_payload._id }, (err, admin) => {
+			if (err) {
+				return done(err, false);
+			}
+			if (admin) {
+				return done(null, admin);
+			} else {
+				return done(null, false);
+			}
+		});
+	})
+);
 
 export default passport;
